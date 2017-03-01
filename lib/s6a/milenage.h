0523d99b5b28b867ba1a00b665ae7ff098a55585
Sukchan Lee <acetcom@gmail.com>
2017-03-01 22:10:09 +0900

    milenage algorithm function is added

---
+++ lib/s6a/milenage.h
@@ -0,0 +1,33 @@
+/*
+ * UMTS AKA - Milenage algorithm (3GPP TS 35.205, .206, .207, .208)
+ * Copyright (c) 2006-2007 <j@w1.fi>
+ *
+ * This software may be distributed under the terms of the BSD license.
+ * See README for more details.
+ */
+
+#ifndef MILENAGE_H
+#define MILENAGE_H
+
+#include "core.h"
+
+void milenage_generate(const c_uint8_t *opc, const c_uint8_t *amf, 
+    const c_uint8_t *k, const c_uint8_t *sqn, const c_uint8_t *_rand, 
+    c_uint8_t *autn, c_uint8_t *ik, c_uint8_t *ck, c_uint8_t *res, 
+    size_t *res_len);
+int milenage_auts(const c_uint8_t *opc, const c_uint8_t *k, 
+    const c_uint8_t *_rand, const c_uint8_t *auts, c_uint8_t *sqn);
+int gsm_milenage(const c_uint8_t *opc, const c_uint8_t *k, 
+    const c_uint8_t *_rand, c_uint8_t *sres, c_uint8_t *kc);
+int milenage_check(const c_uint8_t *opc, const c_uint8_t *k, 
+    const c_uint8_t *sqn, const c_uint8_t *_rand, const c_uint8_t *autn, 
+    c_uint8_t *ik, c_uint8_t *ck, c_uint8_t *res, size_t *res_len,
+    c_uint8_t *auts);
+int milenage_f1(const c_uint8_t *opc, const c_uint8_t *k, 
+    const c_uint8_t *_rand, const c_uint8_t *sqn, const c_uint8_t *amf, 
+    c_uint8_t *mac_a, c_uint8_t *mac_s);
+int milenage_f2345(const c_uint8_t *opc, const c_uint8_t *k, 
+    const c_uint8_t *_rand, c_uint8_t *res, c_uint8_t *ck, c_uint8_t *ik, 
+    c_uint8_t *ak, c_uint8_t *akstar);
+
+#endif /* MILENAGE_H */
