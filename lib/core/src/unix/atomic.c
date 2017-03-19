ef14471e0f908c35fcef8ad48eb84ef51927413f
Sukchan Lee <acetcom@gmail.com>
2017-03-19 21:50:43 +0900

    GCC builtin atomic function is added

--- lib/core/src/unix/atomic.c
+++ lib/core/src/unix/atomic.c
@@ -4,6 +4,75 @@
 #include "core_mutex.h"
 #include "core_debug.h"
 
+#if HAVE_ATOMIC_BUILTINS == 1
+
+status_t atomic_init()
+{
+    return CORE_OK;
+}
+
+status_t atomic_final()
+{
+    return CORE_OK;
+}
+
+c_uint32_t atomic_read32(volatile c_uint32_t *mem)
+{
+    return *mem;
+}
+
+void atomic_set32(volatile c_uint32_t *mem, c_uint32_t val)
+{
+    *mem = val;
+}
+
+c_uint32_t atomic_add32(volatile c_uint32_t *mem, c_uint32_t val)
+{
+    return __sync_fetch_and_add(mem, val);
+}
+
+void atomic_sub32(volatile c_uint32_t *mem, c_uint32_t val)
+{
+    __sync_fetch_and_sub(mem, val);
+}
+
+c_uint32_t atomic_inc32(volatile c_uint32_t *mem)
+{
+    return __sync_fetch_and_add(mem, 1);
+}
+
+int atomic_dec32(volatile c_uint32_t *mem)
+{
+    return __sync_sub_and_fetch(mem, 1);
+}
+
+c_uint32_t atomic_cas32(volatile c_uint32_t *mem, c_uint32_t with,
+                                           c_uint32_t cmp)
+{
+    return __sync_val_compare_and_swap(mem, cmp, with);
+}
+
+c_uint32_t atomic_xchg32(volatile c_uint32_t *mem, c_uint32_t val)
+{
+    __sync_synchronize();
+
+    return __sync_lock_test_and_set(mem, val);
+}
+
+void* atomic_casptr(void *volatile *mem, void *with, const void *cmp)
+{
+    return (void*) __sync_val_compare_and_swap(mem, (void *)cmp, with);
+}
+
+void* atomic_xchgptr(void *volatile *mem, void *with)
+{
+    __sync_synchronize();
+
+    return (void*) __sync_lock_test_and_set(mem, with);
+}
+
+#else
+
 #define DECLARE_MUTEX_LOCKED(name, mem) \
         mutex_id name = mutex_hash(mem)
 #define MUTEX_UNLOCK(name) \
@@ -166,3 +235,4 @@ void* atomic_xchgptr(void *volatile *mem, void *with)
 
     return prev;
 }
+#endif
