f6aeefd6472d3f30ed89b383b474cc4bfe081fd4
Sukchan Lee <acetcom@gmail.com>
2017-03-27 19:32:39 +0900

    update it

---
+++ src/mme/s11_sm.c
@@ -0,0 +1,78 @@
+#define TRACE_MODULE _s11_sm
+
+#include "core_debug.h"
+
+#include "event.h"
+
+void s11_state_initial(s11_sm_t *s, event_t *e)
+{
+    d_assert(s, return, "Null param");
+
+    mme_sm_trace(1, e);
+
+    FSM_TRAN(s, &s11_state_operational);
+}
+
+void s11_state_final(s11_sm_t *s, event_t *e)
+{
+    d_assert(s, return, "Null param");
+
+    mme_sm_trace(1, e);
+}
+
+void s11_state_operational(s11_sm_t *s, event_t *e)
+{
+    d_assert(s, return, "Null param");
+    d_assert(e, return, "Null param");
+
+    ue_ctx_t *ue = s->ctx;
+    d_assert(ue, return, "Null param");
+
+    mme_sm_trace(1, e);
+
+    switch (event_get(e))
+    {
+        case FSM_ENTRY_SIG:
+        {
+            break;
+        }
+        case FSM_EXIT_SIG:
+        {
+            break;
+        }
+        case EVT_MSG_MME_S11:
+        {
+            break;
+        }
+        default:
+        {
+            d_error("Unknown event %s", mme_event_get_name(e));
+            break;
+        }
+    }
+}
+
+void s11_state_exception(s11_sm_t *s, event_t *e)
+{
+    d_assert(s, return, "Null param");
+    d_assert(e, return, "Null param");
+
+    mme_sm_trace(1, e);
+
+    switch (event_get(e))
+    {
+        case FSM_ENTRY_SIG:
+        {
+            break;
+        }
+        case FSM_EXIT_SIG:
+        {
+            break;
+        }
+        default:
+        {
+            d_error("Unknown event %s", mme_event_get_name(e));
+            break;
+        }
+    }
+}
