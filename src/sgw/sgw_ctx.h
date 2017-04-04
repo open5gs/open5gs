8db9d1ce826ae0b37f71e1bf5516fc97e009ac97
Sukchan Lee <acetcom@gmail.com>
2017-04-04 16:46:40 +0900

    update it

--- src/sgw/sgw_ctx.h
+++ src/sgw/sgw_ctx.h
@@ -35,36 +35,10 @@ typedef struct _sgw_ctx_t {
     gtp_xact_ctx_t  gtp_xact_ctx;   /* GTP Transaction Context */
 } sgw_ctx_t;
 
-typedef struct _s11_ctx_t {
-    lnode_t         node; /**< A node of list_t */
-
-    c_uint32_t      teid;
-} s11_ctx_t;
-
-typedef struct _s5c_ctx_t {
-    lnode_t         node; /**< A node of list_t */
-
-    c_uint32_t      teid;
-} s5c_ctx_t;
-
 CORE_DECLARE(status_t)      sgw_ctx_init(void);
 CORE_DECLARE(status_t)      sgw_ctx_final(void);
 
 CORE_DECLARE(sgw_ctx_t*)    sgw_self(void);
-
-CORE_DECLARE(s11_ctx_t*)    sgw_ctx_s11_add();
-CORE_DECLARE(status_t)      sgw_ctx_s11_remove(s11_ctx_t *s11);
-CORE_DECLARE(status_t)      sgw_ctx_s11_remove_all();
-CORE_DECLARE(s11_ctx_t*)    sgw_ctx_s11_find_by_teid(c_uint32_t teid);
-CORE_DECLARE(s11_ctx_t*)    sgw_ctx_s11_first();
-CORE_DECLARE(s11_ctx_t*)    sgw_ctx_s11_next(s11_ctx_t *s11);
-
-CORE_DECLARE(s5c_ctx_t*)    sgw_ctx_s5c_add();
-CORE_DECLARE(status_t)      sgw_ctx_s5c_remove(s5c_ctx_t *s5c);
-CORE_DECLARE(status_t)      sgw_ctx_s5c_remove_all();
-CORE_DECLARE(s5c_ctx_t*)    sgw_ctx_s5c_find_by_teid(c_uint32_t teid);
-CORE_DECLARE(s5c_ctx_t*)    sgw_ctx_s5c_first();
-CORE_DECLARE(s5c_ctx_t*)    sgw_ctx_s5c_next(s5c_ctx_t *s5c);
 
 #ifdef __cplusplus
 }
