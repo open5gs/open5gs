39e8d5cbfb3ac015b69b985d27d89e0273b6b554
Sukchan Lee <acetcom@gmail.com>
2017-05-27 00:20:42 +0900

    Suggest Initial View

--- webui/components/App.js
+++ webui/components/App.js
@@ -4,46 +4,10 @@ import Head from 'next/head';
 import styled from 'styled-components';
 import oc from 'open-color';
 import { media, transitions } from '../lib/style-utils';
-import MenuIcon from 'react-icons/lib/md/menu';
-import Thumbnail from './Thumbnail';
+
+import Header from './Header';
 
 import Session from '../lib/session';
-import LogoutButton from '../components/logout-button';
-
-const Header = styled.div`
-  position: relative;
-  display: flex;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
-  z-index: 2;
-
-  background: ${oc.indigo[6]};
-  color: white;
-  border-bottom: 1px solid ${oc.indigo[7]};
-  box-shadow: 0 3px 6px rgba(0,0,0,0.10), 0 3px 6px rgba(0,0,0,0.20);
-`;
-
-const HeaderMenu = styled.div`
-  width: 3.0rem;
-
-  text-align: center;
-  font-size: 1.5rem;
-`;
-
-const HeaderTitle = styled.div`
-  padding-left: 1rem;
-  padding-top: 0.2rem;
-
-  font-size: 1.5rem;
-  font-weight: 200;
-`;
-
-const HeaderThumbnail = styled.div`
-  position: absolute;
-  right: 1rem;
-
-  font-size: 1.5rem;
-`;
 
 const BodyContainer = styled.div`
   display: flex;
@@ -95,9 +59,6 @@ const Footer = styled.div`
   background-color: white;
 `;
 
-const TempWrapper = styled.div`
-`;
-
 const App = ({ session }) => {
   const title = 'Next, EPC ' + Package.version;
 
@@ -107,17 +68,7 @@ const App = ({ session }) => {
         <title>{title}</title>
       </Head>
 
-      <Header>
-        <HeaderMenu>
-          <MenuIcon/>
-        </HeaderMenu>
-        <HeaderTitle>
-          Next, EPC
-        </HeaderTitle>
-        <HeaderThumbnail>
-          <Thumbnail size="2rem" color={oc['pink'][4]} />
-        </HeaderThumbnail>
-      </Header>
+      <Header/>
       <BodyContainer>
         <Sidebar/>
         <ContentContainer>
@@ -131,11 +82,6 @@ const App = ({ session }) => {
           </Footer>
         </ContentContainer>
       </BodyContainer>
-
-      <TempWrapper>
-      <p>Welcome back {session.user.username}</p>
-      <LogoutButton session={session}>Log out</LogoutButton>
-      </TempWrapper>
     </div>
   )
 }
--- webui/components/Header.js
+++ webui/components/Header.js
@@ -1,21 +1,75 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import styled from 'styled-components';
 import oc from 'open-color';
 
+import MenuIcon from 'react-icons/lib/md/menu';
+import ThumbnailIcon from './Thumbnail';
+
+import Session from '../lib/session';
+
 const Wrapper = styled.div`
-  position: absolute;
-  margin: 0 auto;
-  width: 100%;
+  position: relative;
+  display: flex;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  z-index: 2;
 
   background: ${oc.indigo[6]};
   color: white;
   border-bottom: 1px solid ${oc.indigo[7]};
   box-shadow: 0 3px 6px rgba(0,0,0,0.10), 0 3px 6px rgba(0,0,0,0.20);
-
-  font-size: 2.5rem;
 `;
 
-export default () => (
-  <Wrapper>
-     NextEPC
- </Wrapper>
-)
+const Menu = styled.div`
+  width: 3.0rem;
+
+  text-align: center;
+  font-size: 1.5rem;
+`;
+
+const Title = styled.div`
+  padding-left: 1rem;
+  padding-top: 0.2rem;
+
+  font-size: 1.5rem;
+  font-weight: 200;
+`;
+
+const Thumbnail = styled.div`
+  position: absolute;
+  right: 1rem;
+
+  font-size: 1.5rem;
+`;
+
+async function logout (e) {
+  e.preventDefault()
+
+  const session = new Session()
+  await session.signout()
+
+  // @FIXME next/router not working reliably  so using window.location
+  window.location = '/'
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Menu>
+          <MenuIcon/>
+        </Menu>
+        <Title>
+          Next, EPC
+        </Title>
+        <Thumbnail onClick={logout}>
+          <ThumbnailIcon size="2rem" color={oc['pink'][4]} />
+        </Thumbnail>
+    </Wrapper>
+    )
+  }
+}
+
+export default Header;
