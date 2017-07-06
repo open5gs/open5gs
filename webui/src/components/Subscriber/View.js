8d015f3038afe3e2ef136ae7c605f94795dc7170
Sukchan Lee <acetcom@gmail.com>
2017-07-06 21:55:34 +0900

    view css design done

--- webui/src/components/Subscriber/View.js
+++ webui/src/components/Subscriber/View.js
@@ -8,6 +8,10 @@ import EditIcon from 'react-icons/lib/md/edit';
 import DeleteIcon from 'react-icons/lib/md/delete';
 import CloseIcon from 'react-icons/lib/md/close';
 
+import SecurityIcon from 'react-icons/lib/md/security';
+import PdnIcon from 'react-icons/lib/md/cast';
+import KeyboardControlIcon from 'react-icons/lib/md/keyboard-control';
+
 import { Modal, Tooltip, Heading, Text, LineSeparator } from 'components';
 
 const Wrapper = styled.div`
@@ -30,6 +34,12 @@ const Header = styled.div`
 
   background: ${oc.gray[1]};
 
+  .title {
+    padding: 1.5rem;
+    color: ${oc.gray[8]};
+    font-size: 1.5rem;
+  }
+
   .actions {
     position: absolute;
     top: 0;
@@ -40,12 +50,6 @@ const Header = styled.div`
     align-items: center;
     justify-content: center;
   }
-`;
-
-const Imsi = styled.div`
-  padding: 1.5rem;
-  color: ${oc.gray[8]};
-  font-size: 1.5rem;
 `;
 
 const CircleButton = styled.div`
@@ -74,9 +78,9 @@ const CircleButton = styled.div`
 
 const Body = styled.div`
   display: block;
-  margin: 0.5rem 1rem;
+  margin: 0.5rem;
 
-  height: 300px;
+  height: 320px;
   ${media.mobile`
     height: calc(100vh - 16rem);
   `}
@@ -84,33 +88,67 @@ const Body = styled.div`
   overflow: scroll;
 `
 
-const View = ({ visible, disableOnClickOutside, subscriber, onEdit, onDelete, onHide }) => {
-  const imsi = (subscriber || {}).imsi;
-  const security = ((subscriber || {}).security || {});
-  const ue_ambr = ((subscriber || {}).ue_ambr || {});
-  const pdns = ((subscriber || {}).pdn || []);
+const Subscriber = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0, auto;
+  color: ${oc.gray[9]};
 
-  return (
-    <Modal 
-      visible={visible} 
-      onOutside={onHide}
-      disableOnClickOutside={disableOnClickOutside}>
-      <Wrapper>
-        <Header>
-          <Imsi>{imsi}</Imsi>
-          <div className="actions">
-            <Tooltip content='Edit' width="60px">
-              <CircleButton onClick={() => onEdit(imsi)}><EditIcon/></CircleButton>
-            </Tooltip>
-            <Tooltip content='Delete' width="60px">
-              <CircleButton className="delete" onClick={() => onDelete(imsi)}><DeleteIcon/></CircleButton>
-            </Tooltip>
-            <Tooltip content='Close' width="60px">
-              <CircleButton className="delete" onClick={onHide}><CloseIcon/></CircleButton>
-            </Tooltip>
-          </div>
-        </Header>
-        <Body>
+  .header {
+    margin: 12px;
+    font-size: 16px;
+  }
+  .body {
+    display: flex;
+    flex-direction: row;
+    flex:1;
+    margin: 6px;
+
+    .left {
+      width: 80px;
+      text-align: center;
+      font-size: 18px;
+      color: ${oc.gray[6]};
+    }
+
+    .right {
+      display: flex;
+      flex-direction: column;
+      flex:1;
+
+      .data {
+        flex:1;
+        font-size: 12px;
+        margin: 4px;
+      }
+    }
+  }
+`
+
+const Pdn = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  color: ${oc.gray[9]};
+
+  .header {
+    margin: 12px;
+    font-size: 16px;
+  }
+  .body {
+    display: flex;
+    flex-direction: row;
+    flex:1;
+    margin: 0px 32px;
+
+    .data {
+      width: 80px;
+      font-size: 12px;
+      margin: 4px;
+    }
+  }
+`
+/*
           <Heading el='h3'>Security</Heading>
           <Text>K : {security.k}</Text>
           <Text>OP : {security.op}</Text>
@@ -129,6 +167,93 @@ const View = ({ visible, disableOnClickOutside, subscriber, onEdit, onDelete, on
               <Text>AMBR : {pdn.pdn_ambr.max_bandwidth_ul}/{pdn.pdn_ambr.max_bandwidth_ul}</Text>
             </div>
           )}
+          */
+
+const View = ({ visible, disableOnClickOutside, subscriber, onEdit, onDelete, onHide }) => {
+  const imsi = (subscriber || {}).imsi;
+  const security = ((subscriber || {}).security || {});
+  const ue_ambr = ((subscriber || {}).ue_ambr || {});
+  const pdns = ((subscriber || {}).pdn || []);
+
+  return (
+    <Modal 
+      visible={visible} 
+      onOutside={onHide}
+      disableOnClickOutside={disableOnClickOutside}>
+      <Wrapper>
+        <Header>
+          <div className="title">{imsi}</div>
+          <div className="actions">
+            <Tooltip content='Edit' width="60px">
+              <CircleButton onClick={() => onEdit(imsi)}><EditIcon/></CircleButton>
+            </Tooltip>
+            <Tooltip content='Delete' width="60px">
+              <CircleButton className="delete" onClick={() => onDelete(imsi)}><DeleteIcon/></CircleButton>
+            </Tooltip>
+            <Tooltip content='Close' width="60px">
+              <CircleButton className="delete" onClick={onHide}><CloseIcon/></CircleButton>
+            </Tooltip>
+          </div>
+        </Header>
+        <Body>
+          <Subscriber>
+            <div className="header">
+              Subscriber Details
+            </div>
+            <div className="body">
+              <div className="left">
+                <SecurityIcon/>
+              </div>
+              <div className="right">
+                <div className="data">
+                  {security.k}
+                  <span style={{color:oc.gray[5]}}><KeyboardControlIcon/>K</span>
+                </div>
+                <div className="data">
+                  {security.op}
+                  <span style={{color:oc.gray[5]}}><KeyboardControlIcon/>OP</span>
+                </div>
+                <div className="data">
+                  {security.amf}
+                  <span style={{color:oc.gray[5]}}><KeyboardControlIcon/>AMF</span>
+                </div>
+              </div>
+            </div>
+            <div className="body">
+              <div className="left">
+                <PdnIcon/>
+              </div>
+              <div className="right">
+                <div className="data">
+                  {ue_ambr.max_bandwidth_ul} Kbps
+                  <span style={{color:oc.gray[5]}}><KeyboardControlIcon/>UL</span>
+                </div>
+                <div className="data">
+                  {ue_ambr.max_bandwidth_dl} Kbps
+                  <span style={{color:oc.gray[5]}}><KeyboardControlIcon/>DL</span>
+                </div>
+              </div>
+            </div>
+          </Subscriber>
+          <Pdn>
+            <div className="header">
+              PDN
+            </div>
+            <div className="body" style={{color:oc.gray[5]}}>
+              <div className="data">APN</div>
+              <div className="data">QCI</div>
+              <div className="data">ARP</div>
+              <div className="data">UL/DL(Kbps)</div>
+            </div>
+            {pdns.map(pdn =>
+              <div className="body" key={pdn.apn}>
+                <div className="data">{pdn.apn}</div>
+                <div className="data">{pdn.qos.qci}</div>
+                <div className="data">{pdn.qos.arp.priority_level}</div>
+                <div className="data">{pdn.pdn_ambr.max_bandwidth_ul}/{pdn.pdn_ambr.max_bandwidth_ul}</div>
+              </div>
+            )}
+          </Pdn>
         </Body>
       </Wrapper>
     </Modal>
