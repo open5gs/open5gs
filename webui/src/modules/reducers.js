10542373b2cc6f47506c019e089ea98b71d95a09
Sukchan Lee <acetcom@gmail.com>
2017-07-07 20:59:07 +0900

    remove reduant code

--- webui/src/modules/reducers.js
+++ webui/src/modules/reducers.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 
-import auth from './auth/reducers';
 import crud from './crud/reducers';
 import sidebar from './sidebar';
 import notifications from './notification/reducers';
 
 export default combineReducers({
-  auth,
   crud,
   sidebar,
   notifications
--- webui/src/modules/sagas.js
+++ webui/src/modules/sagas.js
@@ -1,10 +1,8 @@
 import { all, fork } from 'redux-saga/effects';
-import auth from './auth/sagas';
 import crud from './crud/sagas';
 
 export default function* rootSaga() {
   yield all([
-    fork(auth),
     fork(crud)
   ])
 }
