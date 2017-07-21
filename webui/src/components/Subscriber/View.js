b7020842c66788f654352388d95e713808a04554
Sukchan Lee <acetcom@gmail.com>
2017-07-21 09:32:12 +0900

    update it

--- webui/src/components/Subscriber/View.js
+++ webui/src/components/Subscriber/View.js
@@ -197,6 +197,12 @@ const View = ({ visible, disableOnClickOutside, subscriber, onEdit, onDelete, on
                     {security.amf}
                     <span style={{color:oc.gray[5]}}><KeyboardControlIcon/>AMF</span>
                   </div>
+                  {security.rand &&
+                    <div className="data">
+                      {security.rand}
+                      <span style={{color:oc.gray[5]}}><KeyboardControlIcon/>RAND</span>
+                    </div>
+                  }
                 </div>
               </div>
               <div className="body">
