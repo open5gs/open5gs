f6f1720fee1f8d93d41f58a19e223f8d0020a7ce
Sukchan Lee <acetcom@gmail.com>
2017-10-20 23:51:54 +0900

    update netconfig.sh

--- support/netconfig.sh
+++ support/netconfig.sh
@@ -8,10 +8,22 @@ if [ "$SYSTEM" = "Linux" ]; then
     ifconfig lo:sgw 10.1.35.216 netmask 255.255.255.255 up
     ifconfig lo:pcrf 10.1.35.218 netmask 255.255.255.255 up
     ifconfig lo:pgw 10.1.35.219 netmask 255.255.255.255 up
-else
+elif [ "$SYSTEM" = "Darwin" ]; then
     ifconfig lo0 alias 10.1.35.214 netmask 255.255.255.255
     ifconfig lo0 alias 10.1.35.215 netmask 255.255.255.255
     ifconfig lo0 alias 10.1.35.216 netmask 255.255.255.255
     ifconfig lo0 alias 10.1.35.218 netmask 255.255.255.255
     ifconfig lo0 alias 10.1.35.219 netmask 255.255.255.255
+else
+    if [ 1 -ne $# ]
+    then
+        echo You must specify output directory : ./netconfig.sh em0
+
+        exit;
+    fi
+    ifconfig $1 alias 10.1.35.214 netmask 255.255.255.255
+    ifconfig $1 alias 10.1.35.215 netmask 255.255.255.255
+    ifconfig $1 alias 10.1.35.216 netmask 255.255.255.255
+    ifconfig $1 alias 10.1.35.218 netmask 255.255.255.255
+    ifconfig $1 alias 10.1.35.219 netmask 255.255.255.255
 fi
