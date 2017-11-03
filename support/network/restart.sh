f35eea1634404db61a956544b4b1195b80ac3d66
Sukchan Lee <acetcom@gmail.com>
2017-11-03 10:48:38 +0900

    add favicon in Web User Interface

--- support/network/restart.sh
+++ support/network/restart.sh
@@ -5,9 +5,9 @@ SYSTEM=`uname`;
 if [ "$SYSTEM" = "Linux" ]; then
     if ! grep "pgwtun" /proc/net/dev > /dev/null; then
         ip tuntap add name pgwtun mode tun
-        ip addr add 45.45.0.1/16 dev pgwtun
-        ip link set pgwtun up
     fi
+    ip addr add 45.45.0.1/16 dev pgwtun
+    ip link set pgwtun up
 else
     ifconfig lo0 alias 127.76.0.1 netmask 255.255.255.255
     ifconfig lo0 alias 127.76.0.2 netmask 255.255.255.255
