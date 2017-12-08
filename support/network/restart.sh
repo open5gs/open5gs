f069f19185b5d626c3878bc9a69a526343d6868f
Sukchan Lee <acetcom@gmail.com>
2017-12-08 06:30:15 +0000

    rollback

--- support/network/restart.sh
+++ support/network/restart.sh
@@ -8,17 +8,17 @@ if [ "$SYSTEM" = "Linux" ]; then
     fi
     ip addr add 45.45.0.1/16 dev pgwtun
     ip link set pgwtun up
-    ip addr add fe80::2 dev lo
-    ip addr add fe80::3 dev lo
-    ip addr add fe80::4 dev lo
-    ip addr add fe80::5 dev lo
+    ip addr add fec0::2 dev lo
+    ip addr add fec0::3 dev lo
+    ip addr add fec0::4 dev lo
+    ip addr add fec0::5 dev lo
 else
     ifconfig lo0 alias 127.0.0.2 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.3 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.4 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.5 netmask 255.255.255.255
-    ifconfig lo0 inet6 fe80::2 prefixlen 128
-    ifconfig lo0 inet6 fe80::3 prefixlen 128
-    ifconfig lo0 inet6 fe80::4 prefixlen 128
-    ifconfig lo0 inet6 fe80::5 prefixlen 128
+    ifconfig lo0 inet6 fec0::2 prefixlen 128
+    ifconfig lo0 inet6 fec0::3 prefixlen 128
+    ifconfig lo0 inet6 fec0::4 prefixlen 128
+    ifconfig lo0 inet6 fec0::5 prefixlen 128
 fi
