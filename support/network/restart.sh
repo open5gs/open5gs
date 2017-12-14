05c95dbfefcd7bc093858fa04d6b02f7befba50f
Sukchan Lee <acetcom@gmail.com>
2017-12-14 01:46:35 +0000

    default address is changed.
    o PGW
     - GTPC : 127.0.0.1, [::1]
     - GTPU : 127.0.0.1, [::1]
    
    o SGW
     - GTPC : 127.0.0.2
     - GTPU : auto-configured
    
    o MME
     - S1AP : auto-configured
     - GTPC : auto-configured
     - Diameter : 127.0.0.3
    
    o HSS
     - Diameter : 127.0.0.4
    
    o PCRF
     - Diameter : 127.0.0.5

--- support/network/restart.sh
+++ support/network/restart.sh
@@ -11,25 +11,9 @@ if [ "$SYSTEM" = "Linux" ]; then
     ip addr del cafe::1/64 dev pgwtun 2> /dev/null
     ip addr add cafe::1/64 dev pgwtun
     ip link set pgwtun up
-    ip addr del fe80::2 dev lo 2> /dev/null
-    ip addr del fe80::3 dev lo 2> /dev/null
-    ip addr del fe80::4 dev lo 2> /dev/null
-    ip addr del fe80::5 dev lo 2> /dev/null
-    ip addr add fe80::2 dev lo
-    ip addr add fe80::3 dev lo
-    ip addr add fe80::4 dev lo
-    ip addr add fe80::5 dev lo
 else
     ifconfig lo0 alias 127.0.0.2 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.3 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.4 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.5 netmask 255.255.255.255
-    ifconfig lo0 inet6 delete fe80::2 prefixlen 128 2> /dev/null
-    ifconfig lo0 inet6 delete fe80::3 prefixlen 128 2> /dev/null
-    ifconfig lo0 inet6 delete fe80::4 prefixlen 128 2> /dev/null
-    ifconfig lo0 inet6 delete fe80::5 prefixlen 128 2> /dev/null
-    ifconfig lo0 inet6 add fe80::2 prefixlen 128
-    ifconfig lo0 inet6 add fe80::3 prefixlen 128
-    ifconfig lo0 inet6 add fe80::4 prefixlen 128
-    ifconfig lo0 inet6 add fe80::5 prefixlen 128
 fi
