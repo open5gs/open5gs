081170a86e71e59dfae91d3beda7039384593485
Sukchan Lee <acetcom@gmail.com>
2018-02-13 16:15:11 +0900

    add template Dockerfile for NextEPC

--- support/docker/build-freediameter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir -p /root/build-freediameter/freeDiameter
-wget -q -O - http://www.freediameter.net/hg/freeDiameter/archive/2cb8d71a405d.tar.bz2 | tar xj -C build-freediameter/freeDiameter --strip-components=1
-cd /root/build-freediameter/freeDiameter
-ln -s contrib/debian .
-fakeroot dh binary
