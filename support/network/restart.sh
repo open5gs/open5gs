1009ac5e47deedc2fb7bcc918452d4bfa550a4b3
Sukchan Lee <acetcom@gmail.com>
2018-02-14 20:09:04 +0900

    add masquerading for Mac OS X

--- support/network/restart.sh
+++ support/network/restart.sh
@@ -12,8 +12,15 @@ if [ "$SYSTEM" = "Linux" ]; then
     ip addr add cafe::1/64 dev pgwtun
     ip link set pgwtun up
 else
+    sysctl -w net.inet.ip.forwarding=1
     ifconfig lo0 alias 127.0.0.2 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.3 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.4 netmask 255.255.255.255
     ifconfig lo0 alias 127.0.0.5 netmask 255.255.255.255
+    if [ "$SYSTEM" = "Darwin" ]; then
+        if ! test -f /etc/pf.anchors/org.nextepc; then
+            sudo sh -c "echo 'nat on {en0} from 45.45.0.0/16 to any -> {en0}' > /etc/pf.anchors/org.nextepc"
+        fi
+        pfctl -e -f /etc/pf.anchors/org.nextepc
+    fi
 fi
