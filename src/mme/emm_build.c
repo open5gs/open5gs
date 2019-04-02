6bd8411fc1cc6901d9a252269a35d13d78de57b6
Sukchan Lee <acetcom@gmail.com>
2019-04-02 11:11:24 +0900

    Add log message if EIA0 is used (#167)

--- src/mme/emm_build.c
+++ src/mme/emm_build.c
@@ -278,13 +278,22 @@ status_t emm_build_security_mode_command(
             sizeof(replayed_ue_security_capabilities->uea) +
             sizeof(replayed_ue_security_capabilities->uia) +
             sizeof(replayed_ue_security_capabilities->gea);
-    d_trace(5, "    SEC[LEN:%d EEA:0x%x EIA:0x%x UEA:0x%x UIA:0x%x GEA:0x%x]\n",
+    d_trace(5, "    UE[LEN:%d EEA:0x%x EIA:0x%x UEA:0x%x UIA:0x%x GEA:0x%x]\n",
             replayed_ue_security_capabilities->length,
             replayed_ue_security_capabilities->eea,
             replayed_ue_security_capabilities->eia,
             replayed_ue_security_capabilities->uea,
             replayed_ue_security_capabilities->uia,
             replayed_ue_security_capabilities->gea);
+    d_trace(5, "    Selected[Integrity:0x%x Encrypt:0x%x]\n",
+            mme_ue->selected_int_algorithm, mme_ue->selected_enc_algorithm);
+    if (mme_ue->selected_int_algorithm == NAS_SECURITY_ALGORITHMS_EIA0)
+    {
+        d_fatal("Encrypt[0x%x] CAN BE skipped, "
+            "but Integrity[0x%x] CANNOT BE skipped",
+            mme_ue->selected_enc_algorithm, mme_ue->selected_int_algorithm);
+        return CORE_ERROR;
+    }
 
     mme_kdf_nas(MME_KDF_NAS_INT_ALG, mme_ue->selected_int_algorithm,
             mme_ue->kasme, mme_ue->knas_int);
