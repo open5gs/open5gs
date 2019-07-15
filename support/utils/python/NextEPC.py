964e7ecebd0ed7670c72300fa2cf8d0c8aca35fe
Nick <nick@nickvsnetworking.com>
2019-07-15 18:29:22 +1000

    Add files via upload
    Basic Python Library / Class for adding / removing / reading data from NextEPC HSS (MongoDB)

---
+++ support/utils/python/NextEPC.py
@@ -0,0 +1,55 @@
+import mongo
+import pymongo
+import random
+import bson
+
+class NextEPC:
+    def __init__(self, server, port):
+        self.server = server
+        self.port = port
+
+
+    def GetSubscribers(self):
+        myclient = pymongo.MongoClient("mongodb://" + str(self.server) + ":" + str(self.port) + "/")
+        mydb = myclient["nextepc"]
+        mycol = mydb["subscribers"]
+        subs_list = []
+        for x in mycol.find():
+            print(x)
+            subs_list.append(x)
+            pass
+
+        return subs_list
+
+    def GetSubscriber(self, imsi):
+        myclient = pymongo.MongoClient("mongodb://" + str(self.server) + ":" + str(self.port) + "/")
+        mydb = myclient["nextepc"]
+        mycol = mydb["subscribers"]
+        myquery = { "imsi": str(imsi)}
+        mydoc = mycol.find(myquery)
+        for x in mydoc:
+            print(x)
+            return x
+        
+
+    def AddSubscriber(self, sub_data):
+
+        myclient = pymongo.MongoClient("mongodb://" + str(self.server) + ":" + str(self.port) + "/")
+        mydb = myclient["nextepc"]
+        mycol = mydb["subscribers"]
+        
+        x = mycol.insert_one(sub_data)
+        print("Added subscriber with Inserted ID : " + str(x.inserted_id))
+        return x.inserted_id
+
+
+    def DeleteSubscriber(self, imsi):
+        myclient = pymongo.MongoClient("mongodb://" + str(self.server) + ":" + str(self.port) + "/")
+        mydb = myclient["nextepc"]
+        mycol = mydb["subscribers"]
+        myquery = { "imsi": str(imsi)}
+        x = mycol.delete_many(myquery)
+        print(x.deleted_count, " subscribers deleted.")
+        return x.deleted_count
+        
+
