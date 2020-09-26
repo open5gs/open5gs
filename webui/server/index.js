b01f6dbd0d8264f952cf4e124878f21c3fc22fe7
Bjoern Riemer <bjoern.c3@nixda.biz>
2020-09-26 14:14:59 +0200

    make webui listen on 0.0.0.0

--- webui/server/index.js
+++ webui/server/index.js
@@ -101,9 +101,9 @@ co(function* () {
     server.use(morgan('tiny'));
   }
 
-  server.listen(3000, err => {
+  server.listen(3000, "0.0.0.0", err => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log('> Ready on http://0.0.0.0:3000');
   });
 })
 .catch(error => console.error(error.stack));
