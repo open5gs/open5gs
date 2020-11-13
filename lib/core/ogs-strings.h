d7c188dc0051bcb2674ad70fd94a23d8bffb033b
Sukchan Lee <acetcom@gmail.com>
2020-11-12 20:58:08 -0500

    License update

--- lib/core/ogs-strings.h
+++ lib/core/ogs-strings.h
@@ -30,7 +30,7 @@
  * This file is part of Open5GS.
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
