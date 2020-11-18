70f7e2d5d4e624d7db5d74bfee69ffc328fa0e6f
R. Medeiros <medeiros@medeiros.eng.br>
2020-11-18 18:33:02 -0300

    Update s5c-handler.c (#674)
    Add information of the used APN in the smf log file

--- src/smf/s5c-handler.c
+++ src/smf/s5c-handler.c
@@ -154,8 +154,9 @@ void smf_s5c_handle_create_session_request(
 
     smf_sess_set_ue_ip(sess);
 
-    ogs_info("UE IMSI:[%s] IPv4:[%s] IPv6:[%s]",
+    ogs_info("UE IMSI:[%s] APN:[%s] IPv4:[%s] IPv6:[%s]",
 	    smf_ue->imsi_bcd,
+	    sess->pdn.apn,
         sess->ipv4 ? OGS_INET_NTOP(&sess->ipv4->addr, buf1) : "",
         sess->ipv6 ? OGS_INET6_NTOP(&sess->ipv6->addr, buf2) : "");
 
