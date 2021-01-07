7dd4609c0f5199b9bc8f8a14e09975aa2af4aa0f
Sukchan Lee <acetcom@gmail.com>
2021-01-07 13:18:45 -0500

    fix: increase mempool to support 1024UEs (#753)

--- lib/core/ogs-pkbuf.c
+++ lib/core/ogs-pkbuf.c
@@ -92,9 +92,9 @@ void ogs_pkbuf_default_init(ogs_pkbuf_config_t *config)
     ogs_assert(config);
     memset(config, 0, sizeof *config);
 
-    config->cluster_128_pool = 32768;
-    config->cluster_256_pool = 4096;
-    config->cluster_512_pool = 2048;
+    config->cluster_128_pool = 65536;
+    config->cluster_256_pool = 8192;
+    config->cluster_512_pool = 4096;
     config->cluster_1024_pool = 1024;
     config->cluster_2048_pool = 512;
     config->cluster_8192_pool = 128;
