5234001b0a7bf5124eafde53c541b087d1dcf7d2
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2021-08-22 19:06:14 -0400

    Increase cluster_2048_pool to 1024 (#1143)

--- lib/core/ogs-pkbuf.c
+++ lib/core/ogs-pkbuf.c
@@ -96,7 +96,7 @@ void ogs_pkbuf_default_init(ogs_pkbuf_config_t *config)
     config->cluster_256_pool = 16384;
     config->cluster_512_pool = 4096;
     config->cluster_1024_pool = 2048;
-    config->cluster_2048_pool = 512;
+    config->cluster_2048_pool = 1024;
     config->cluster_8192_pool = 512;
     config->cluster_big_pool = 8;
 }
