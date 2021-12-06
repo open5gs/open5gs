37787cb25f1e08b37eb27437ce9833b2458d50bb
Kenny Barlee <kbarlee@outlook.com>
2021-12-06 23:58:43 +0000

    change reject cause to 11

--- src/mme/mme-sm.c
+++ src/mme/mme-sm.c
@@ -45,7 +45,7 @@ static uint8_t emm_cause_from_diameter(
         switch (*dia_exp_err) {
         case OGS_DIAM_S6A_ERROR_USER_UNKNOWN:                   /* 5001 */
             ogs_info("[%s] User Unknown in HSS DB", mme_ue->imsi_bcd);
-            return EMM_CAUSE_EPS_SERVICES_AND_NON_EPS_SERVICES_NOT_ALLOWED;
+            return EMM_CAUSE_PLMN_NOT_ALLOWED;
         case OGS_DIAM_S6A_ERROR_UNKNOWN_EPS_SUBSCRIPTION:       /* 5420 */
             /* FIXME: Error diagnostic? */
             return EMM_CAUSE_NO_SUITABLE_CELLS_IN_TRACKING_AREA;
