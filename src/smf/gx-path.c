8286b1c417c8754cb9b91f87831257e2ce9ed87a
Pau Espin Pedrol <pespin@sysmocom.de>
2022-04-06 16:57:55 +0200

    [SMF] Gx: Fix crash receiving DIAMETER_UNABLE_TO_DELIVER (#1469)

--- src/smf/gx-path.c
+++ src/smf/gx-path.c
@@ -30,7 +30,7 @@ struct sess_state {
 
 #define MAX_CC_REQUEST_NUMBER 32
     smf_sess_t *sess;
-    ogs_gtp_xact_t *xact;
+    ogs_gtp_xact_t *xact[MAX_CC_REQUEST_NUMBER];
 
     uint32_t cc_request_type;
     uint32_t cc_request_number;
@@ -185,7 +185,7 @@ void smf_gx_send_ccr(smf_sess_t *sess, ogs_gtp_xact_t *xact,
 
     /* Update session state */
     sess_data->sess = sess;
-    sess_data->xact = xact;
+    sess_data->xact[sess_data->cc_request_number] = xact;
 
     /* Set Origin-Host & Origin-Realm */
     ret = fd_msg_add_origin(req, 0);
@@ -691,7 +691,7 @@ static void smf_gx_cca_cb(void *data, struct msg **msg)
     unsigned long dur;
     int error = 0;
     int new;
-
+    struct msg *req = NULL;
     smf_event_t *e = NULL;
     ogs_gtp_xact_t *xact = NULL;
     smf_sess_t *sess = NULL;
@@ -701,6 +701,10 @@ static void smf_gx_cca_cb(void *data, struct msg **msg)
     ogs_debug("[Credit-Control-Answer]");
 
     ret = clock_gettime(CLOCK_REALTIME, &ts);
+    ogs_assert(ret == 0);
+
+    /* Get originating request of received message, if any */
+    ret = fd_msg_answ_getq(*msg, &req);
     ogs_assert(ret == 0);
 
     /* Search the session, retrieve its data */
@@ -717,7 +721,27 @@ static void smf_gx_cca_cb(void *data, struct msg **msg)
 
     ogs_debug("    Retrieve its data: [%s]", sess_data->gx_sid);
 
-    xact = sess_data->xact;
+    /* Value of CC-Request-Number */
+    ret = fd_msg_search_avp(*msg, ogs_diam_gx_cc_request_number, &avp);
+    ogs_assert(ret == 0);
+    if (!avp && req) {
+        /* Attempt searching for CC-Request-* in original request. Error
+         * messages (like DIAMETER_UNABLE_TO_DELIVER) crafted internally may not
+         * have them. */
+        ret = fd_msg_search_avp(req, ogs_diam_gx_cc_request_number, &avp);
+        ogs_assert(ret == 0);
+    }
+    if (!avp) {
+        ogs_error("no_CC-Request-Number");
+        ogs_assert_if_reached();
+    }
+    ret = fd_msg_avp_hdr(avp, &hdr);
+    ogs_assert(ret == 0);
+    cc_request_number = hdr->avp_value->i32;
+
+    ogs_debug("    CC-Request-Number[%d]", cc_request_number);
+
+    xact = sess_data->xact[cc_request_number];
     ogs_assert(xact);
     sess = sess_data->sess;
     ogs_assert(sess);
@@ -784,36 +808,28 @@ static void smf_gx_cca_cb(void *data, struct msg **msg)
         error++;
     }
 
+    /* Value of CC-Request-Type */
+    ret = fd_msg_search_avp(*msg, ogs_diam_gx_cc_request_type, &avp);
+    ogs_assert(ret == 0);
+    if (!avp && req) {
+        /* Attempt searching for CC-Request-* in original request. Error
+         * messages (like DIAMETER_UNABLE_TO_DELIVER) crafted internally may not
+         * have them. */
+        ret = fd_msg_search_avp(req, ogs_diam_gx_cc_request_type, &avp);
+        ogs_assert(ret == 0);
+    }
+    if (!avp) {
+        ogs_error("no_CC-Request-Number");
+        error++;
+    }
+    ret = fd_msg_avp_hdr(avp, &hdr);
+    ogs_assert(ret == 0);
+    gx_message->cc_request_type = hdr->avp_value->i32;
+
     if (gx_message->result_code != ER_DIAMETER_SUCCESS) {
         ogs_warn("ERROR DIAMETER Result Code(%d)", gx_message->result_code);
         goto out;
     }
-
-    /* Value of CC-Request-Type */
-    ret = fd_msg_search_avp(*msg, ogs_diam_gx_cc_request_type, &avp);
-    ogs_assert(ret == 0);
-    if (avp) {
-        ret = fd_msg_avp_hdr(avp, &hdr);
-        ogs_assert(ret == 0);
-        gx_message->cc_request_type = hdr->avp_value->i32;
-    } else {
-        ogs_error("no_CC-Request-Type");
-        error++;
-    }
-
-    /* Value of CC-Request-Number */
-    ret = fd_msg_search_avp(*msg, ogs_diam_gx_cc_request_number, &avp);
-    ogs_assert(ret == 0);
-    if (avp) {
-        ret = fd_msg_avp_hdr(avp, &hdr);
-        ogs_assert(ret == 0);
-        cc_request_number = hdr->avp_value->i32;
-    } else {
-        ogs_error("no_CC-Request-Number");
-        error++;
-    }
-
-    ogs_debug("    CC-Request-Number[%d]", cc_request_number);
 
     ret = fd_msg_search_avp(*msg, ogs_diam_gx_qos_information, &avp);
     ogs_assert(ret == 0);
