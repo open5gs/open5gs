c8dafd0ea0e9d8701a5fb61459e6f4c59b58690e
jmasterfunk84 <48972964+jmasterfunk84@users.noreply.github.com>
2022-08-15 15:33:52 -0600

    CLR while idle is broken after https://github.com/open5gs/open5gs/commit/7031856cd779af0129ca444e18bbe2ed5db9ff32
    Cancel Location Request arriving while UE is idle will not proceed to paging due to this check for S1 connection.  Using new flag "isAnswer" to bypass this check to allow paging to occur when we are not doing a AIA/ULA related procedure.

--- src/mme/mme-fd-path.c
+++ src/mme/mme-fd-path.c
@@ -233,6 +233,8 @@ static void mme_s6a_aia_cb(void *data, struct msg **msg)
     ogs_assert(aia_message);
     e_utran_vector = &aia_message->e_utran_vector;
     ogs_assert(e_utran_vector);
+
+    s6a_message->isAnswer = true;
     
     /* Value of Result Code */
     ret = fd_msg_search_avp(*msg, ogs_diam_result_code, &avp);
@@ -641,6 +643,8 @@ static void mme_s6a_ula_cb(void *data, struct msg **msg)
     ogs_assert(ula_message);
     subscription_data = &ula_message->subscription_data;
     ogs_assert(subscription_data);
+
+    s6a_message->isAnswer = true;
 
     /* AVP: 'Result-Code'(268)
      * The Result-Code AVP indicates whether a particular request was completed
@@ -1435,7 +1439,9 @@ static int mme_ogs_diam_s6a_clr_cb( struct msg **msg, struct avp *avp,
     ogs_assert(s6a_message);
     s6a_message->cmd_code = OGS_DIAM_S6A_CMD_CODE_CANCEL_LOCATION;
     clr_message = &s6a_message->clr_message;
-    ogs_assert(clr_message);    
+    ogs_assert(clr_message);
+
+    s6a_message->isAnswer = false;
 
     /* Create answer header */
     qry = *msg;
