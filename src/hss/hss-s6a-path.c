83cd3cc7fd72361a539f8581918a1514c68bcb19
jmasterfunk84 <48972964+jmasterfunk84@users.noreply.github.com>
2022-09-12 14:57:17 -0600

    Check for null mme_host and mme_realms
    Do this before trying to compare the strings.

--- src/hss/hss-s6a-path.c
+++ src/hss/hss-s6a-path.c
@@ -795,15 +795,17 @@ static int hss_ogs_diam_s6a_ulr_cb( struct msg **msg, struct avp *avp,
     /* If UE is not purged at MME, determine if the MME sending the ULR  
      * is different from the one that was last used.  if so, send CLR.
      */
-    if (!subscription_data.mme_ispurged) {
-        if ((strcmp(subscription_data.mme_host, mme_host) || 
-            strcmp(subscription_data.mme_realm, mme_realm)) &&
-            strlen(subscription_data.mme_host) > 0 &&
-            strlen(subscription_data.mme_realm) > 0) {
-            hss_s6a_send_clr(imsi_bcd, subscription_data.mme_host, 
-                subscription_data.mme_realm, 
-                OGS_DIAM_S6A_CT_MME_UPDATE_PROCEDURE);
-            ogs_info("[%s] Sending Cancel Location to previous MME", imsi_bcd);
+    if (subscription_data.mme_host != NULL &&
+            subscription_data.mme_realm != NULL) {
+        if (!subscription_data.mme_ispurged) {
+            if (strcmp(subscription_data.mme_host, mme_host) ||
+                strcmp(subscription_data.mme_realm, mme_realm)) {
+                hss_s6a_send_clr(imsi_bcd, subscription_data.mme_host,
+                    subscription_data.mme_realm,
+                    OGS_DIAM_S6A_CT_MME_UPDATE_PROCEDURE);
+                ogs_info("[%s] Sending Cancel Location to previous MME",
+                        imsi_bcd);
+            }
         }
     }
 
