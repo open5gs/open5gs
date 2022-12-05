e3b261eb8bb5be619b3c050220a47736805876d2
Sukchan Lee <acetcom@gmail.com>
2022-12-05 21:30:24 +0900

    [SBI] Case-Insensitive inside MIME message (#1939)

--- lib/sbi/message.c
+++ lib/sbi/message.c
@@ -2070,23 +2070,19 @@ static int on_header_value(
     ogs_assert(data);
 
     if (data->num_of_part < OGS_SBI_MAX_NUM_OF_PART && at && length) {
-        SWITCH(data->header_field)
-        CASE(OGS_SBI_CONTENT_TYPE)
+        if (!ogs_strcasecmp(data->header_field, OGS_SBI_CONTENT_TYPE)) {
             ogs_assert(data->part[data->num_of_part].content_type == NULL);
             data->part[data->num_of_part].content_type =
                 ogs_strndup(at, length);
             ogs_assert(data->part[data->num_of_part].content_type);
-            break;
-        CASE(OGS_SBI_CONTENT_ID)
+        } else if (!ogs_strcasecmp(data->header_field, OGS_SBI_CONTENT_ID)) {
             ogs_assert(data->part[data->num_of_part].content_id == NULL);
             data->part[data->num_of_part].content_id =
                 ogs_strndup(at, length);
             ogs_assert(data->part[data->num_of_part].content_id);
-            break;
-
-        DEFAULT
+        } else {
             ogs_error("Unknown header field [%s]", data->header_field);
-        END
+        }
     }
 
     return 0;
