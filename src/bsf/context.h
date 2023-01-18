7c8627a9c1962fbc3dd6ce7a30db2b62bbe5ad96
mitmitmitm <ois@oasd8i.at>
2023-01-18 12:17:44 +0100

    [BSF] Handle Ipv4FrameRouteList, save it into context

--- src/bsf/context.h
+++ src/bsf/context.h
@@ -52,6 +52,9 @@ typedef struct bsf_sess_s {
     char *ipv4addr_string;
     char *ipv6prefix_string;
 
+    OpenAPI_list_t *ipv4_frame_route_list;
+    OpenAPI_list_t *ipv6_frame_route_list;
+
     uint32_t ipv4addr;
     struct {
         uint8_t len;
