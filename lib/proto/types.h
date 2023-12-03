6728a7686d020d10daec293767855f8a7983ff79
Sukchan Lee <acetcom@gmail.com>
2023-12-03 23:29:23 +0900

    uupdate it

--- lib/proto/types.h
+++ lib/proto/types.h
@@ -88,7 +88,13 @@ extern "C" {
 #define OGS_MAX_NUM_OF_SERVED_GUMMEI    8   /* maxnoofRATs: 8 */
 #define OGS_MAX_NUM_OF_SERVED_GUAMI     256 /* maxnoofServedGUAMIs: 256 */
 #define OGS_MAX_NUM_OF_SUPPORTED_TA     256 /* maxnoofTACs: 256 */
-#define OGS_MAX_NUM_OF_SLICE_SUPPORT    1024 /* maxnoofSliceItems: 1024 */
+
+/*
+ * If I set it to 1024, the AMF crashes in the 'meson test -v registration'.
+ * So for now, I will use 512. Once I figure out the cause of this problem,
+ * I will try 1024.
+ */
+#define OGS_MAX_NUM_OF_SLICE_SUPPORT    512 /* maxnoofSliceItems: 1024 */
 
 #define OGS_MAX_NUM_OF_PLMN_PER_MME     32  /* maxnoofPLMNs(MME): 32 */
 #define OGS_MAX_NUM_OF_PLMN             12  /* maxnoofPLMNs(AMF): 12 */
