9d580d872f0ce9de3e4bf9cf1cdc0a894bc661a4
Gaber Stare <gaber.stare@kontron.si>
2024-06-28 06:58:57 +0200

    [AMF] Fix Allowed NSSAI/SD if SST 0 is used
    Properly encode Allowed NSSAI/SD in Registration
    accept if SST 0 is used.
    
    Solves: https://github.com/open5gs/open5gs/issues/3295

--- lib/nas/5gs/types.c
+++ lib/nas/5gs/types.c
@@ -128,8 +128,7 @@ void ogs_nas_build_s_nssai(
 
     pos = 0;
 
-    if (nas_s_nssai_ie->sst)
-        nas_s_nssai->buffer[pos++] = nas_s_nssai_ie->sst;
+    nas_s_nssai->buffer[pos++] = nas_s_nssai_ie->sst;
 
     if (nas_s_nssai_ie->sd.v != OGS_S_NSSAI_NO_SD_VALUE ||
 
