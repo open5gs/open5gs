c732324409d8f737575f1bd89336e4fdfd7a7a89
albertoberlin <alberto-github@mobileplots.com>
2025-02-18 17:31:53 +0100

    Support User-Agent with format of the type  "<NFType>-<somethingElse>" but still being able to recognize which NF it is (needed at least on SCP). Maybe more optimal would have been to create the array nf_typeArray as str instead of calculating the lengths everytime the function is called

--- lib/sbi/openapi/model/nf_type.c
+++ lib/sbi/openapi/model/nf_type.c
@@ -17,10 +17,12 @@ char* OpenAPI_nf_type_ToString(OpenAPI_nf_type_e nf_type)
 OpenAPI_nf_type_e OpenAPI_nf_type_FromString(char* nf_type)
 {
     int stringToReturn = 0;
+    int len = 0;
     const char *nf_typeArray[] =  { "NULL", "NRF", "UDM", "AMF", "SMF", "AUSF", "NEF", "PCF", "SMSF", "NSSF", "UDR", "LMF", "GMLC", "5G_EIR", "SEPP", "UPF", "N3IWF", "AF", "UDSF", "BSF", "CHF", "NWDAF", "PCSCF", "CBCF", "HSS", "UCMF", "SOR_AF", "SPAF", "MME", "SCSAS", "SCEF", "SCP", "NSSAAF", "ICSCF", "SCSCF", "DRA", "IMS_AS", "AANF", "5G_DDNMF", "NSACF", "MFAF", "EASDF", "DCCF", "MB_SMF", "TSCTSF", "ADRF", "GBA_BSF", "CEF", "MB_UPF", "NSWOF", "PKMF", "MNPF", "SMS_GMSC", "SMS_IWMSC", "MBSF", "MBSTF", "PANF" };
     size_t sizeofArray = sizeof(nf_typeArray) / sizeof(nf_typeArray[0]);
     while (stringToReturn < sizeofArray) {
-        if (strcmp(nf_type, nf_typeArray[stringToReturn]) == 0) {
+        len = strlen (nf_typeArray[stringToReturn]);
+        if (len <= strlen(nf_type) && strncmp(nf_type, nf_typeArray[stringToReturn], len) == 0) {
             return stringToReturn;
         }
         stringToReturn++;
