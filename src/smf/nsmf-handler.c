d6df79328e0b3b26a9e0ba7d2995f486e7ea93d4
Sukchan Lee <acetcom@gmail.com>
2025-06-11 17:28:44 +0900

    update it

--- src/smf/nsmf-handler.c
+++ src/smf/nsmf-handler.c
@@ -714,9 +714,9 @@ bool smf_nsmf_handle_update_sm_context(
     /*
      * <UE-requested PDU Session Release>
      *
-     * 1.  V: OGS_PFCP_MODIFY_HOME_ROUTED_ROAMING|OGS_PFCP_MODIFY_UL_ONLY|
-     *        OGS_PFCP_MODIFY_DEACTIVATE
-     * 2.  V: OGS_PFCP_DELETE_TRIGGER_UE_REQUESTED
+     * 1.  V*: OGS_PFCP_MODIFY_HOME_ROUTED_ROAMING|OGS_PFCP_MODIFY_UL_ONLY|
+     *         OGS_PFCP_MODIFY_DEACTIVATE
+     * 2.  V*: OGS_PFCP_DELETE_TRIGGER_UE_REQUESTED
      * 3.  V: OpenAPI_request_indication_UE_REQ_PDU_SES_REL
      * 4.  V: smf_nsmf_pdusession_build_hsmf_update_data
      * 5.  H: smf_nsmf_handle_update_data_in_hsmf
@@ -726,9 +726,9 @@ bool smf_nsmf_handle_update_sm_context(
      * 8.  H: OGS_FSM_TRAN(s, smf_gsm_state_wait_pfcp_deletion)
      * 9.  H: smf_nsmf_pdusession_build_vsmf_update_data
      * 10. H: OGS_FSM_TRAN(s, smf_gsm_state_wait_5gc_n1_n2_release);
-     * 11. V*: smf_nsmf_handle_update_data_in_vsmf
-     * 12. V*: OpenAPI_request_indication_UE_REQ_PDU_SES_REL
-     * 13. V*: e->h.sbi.state = OGS_PFCP_DELETE_TRIGGER_UE_REQUESTED
+     * 11. V: smf_nsmf_handle_update_data_in_vsmf
+     * 12. V: OpenAPI_request_indication_UE_REQ_PDU_SES_REL
+     * 13. V: e->h.sbi.state = OGS_PFCP_DELETE_TRIGGER_UE_REQUESTED
      * 14. V: OGS_FSM_TRAN(s, smf_gsm_state_wait_pfcp_deletion)
      * 15. V: ngap_build_pdu_session_resource_release_command_transfer+
      *        gsm_build_pdu_session_release_command
