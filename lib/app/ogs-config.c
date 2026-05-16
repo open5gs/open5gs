e4f85aaf6f642549cd3c73220373323e06988330
dchard <metro4@freemail.hu>
2026-05-16 18:04:28 +0200

    [MME] Add network policy (TS 24.301 9.9.3.52)
    This commit adds the network policy IE, part of ATTACH ACCEPT and
    TAU ACCEPT messages. This prevents unsecured redirection to GERAN
    or UTRAN, protecting the UEs from unsecured redirection attacks.
    This IE is mandatory in Rel.19
    
    This patch changes the default behavior!
    
    In case you want to allow unsecured redirects, add the following:
    
    ```
    global:
      parameter:
        no_network_policy: true
    ```
    
    to your mme.yaml

--- lib/app/ogs-config.c
+++ lib/app/ogs-config.c
@@ -259,6 +259,9 @@ int ogs_app_parse_global_conf(ogs_yaml_iter_t *parent)
                 } else if (!strcmp(parameter_key, "no_ims")) {
                     global_conf.parameter.no_ims =
                         ogs_yaml_iter_bool(&parameter_iter);
+                } else if (!strcmp(parameter_key, "no_network_policy")) {
+                    global_conf.parameter.no_network_policy =
+                        ogs_yaml_iter_bool(&parameter_iter);
                 } else if (!strcmp(parameter_key,
                             "no_ipv4v6_local_addr_in_packet_filter")) {
                     global_conf.parameter.
